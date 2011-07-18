d5f266d3f9e26d42ee5bcac94e193dbe6873841d
anealg <anealg@gmail.com>
2011-07-18 09:30:02 -0400

    various updates

--- application/controllers/listings.php
+++ application/controllers/listings.php
@@ -5,19 +5,48 @@ if (!defined('BASEPATH'))
 
 class Listings extends CI_Controller {
 
+    private $meta;
+    private $args;
+
     public function __construct() {
         parent::__construct();
         $this->load->helper('language');
         $this->load->model('listing_model', 'listings');
         $this->load->model('category_model', 'category');
+        $this->$meta['css'] = array('style');
+        $this->$args['categories'] = $this->category->get_all_categories();
+    }
+
+    public function search() {
+        if ($_POST) {
+            $this->$args['search_str'] = $this->input->post('keyword');
+            $this->$args['search_cat'] = $this->input->post('categories');
+        }
+        $this->load->view('header_view', $args);
+
+        if ($_POST) {
+
+            $this->$args['search_items'] = $this->listings->search($this->$args['search_str'], $this->$args['search_cat']);
+        }
+
+
+        $this->load->view('search_view', $this->$args);
+        $this->load->view('footer_view');
     }
 
     public function display() {
-        $meta['css'] = array('style');
-        $args['categories'] = $this->category->get_all_categories();
 
-        $this->load->view('header_view', $meta);
-        $this->load->view('listing_view', $args);
+        $id = base64_decode($this->uri->segment(3));
+
+        switch ($id) {
+            case is_numeric($id):
+                $this->$args['listing'] = $this->listings->get_listing_by_id($id); echo 'cool';
+                break;
+        }
+
+        $this->load->view('header_view', $this->$meta);
+        $this->load->view('listing_view', $this->$args);
         $this->load->view('footer_view');
     }
+
 }
